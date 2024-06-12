d8d2e09db8a1f88efcbeabbd64db6123e65cec29
psy <po2260793@gmail.com>
2024-06-12 16:33:25 +0900

    Replace in-memory storage with MongoDB

---
+++ src/config/db.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+// 비동기 처리를 위한 async 함수
+const connectDB = async () => {
+  try {
+    await mongoose.connect("mongodb://localhost:27017/my-express-server", {
+      useNewUrlParser: true, // 올바른 옵션 이름
+      useUnifiedTopology: true, // 추가 권장 옵션
+    });
+    console.log("MongoDB connected successfully");
+  } catch (err) {
+    console.error("MongoDB connection error:", err.message);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
--- src/strategies/localStrategy.js
+++ src/strategies/localStrategy.js
@@ -1,16 +1,18 @@
-import { Strategy as LocalStrategy } from 'passport-local';
-import { users } from '../users.js';
+import { Strategy as LocalStrategy } from "passport-local";
+import User from "../models/User.js";
 
-const localStrategy = new LocalStrategy({ usernameField: 'username' },
+const localStrategy = new LocalStrategy(
+  { usernameField: "username" },
   async (username, password, done) => {
-    const user = users.find(user => user.username === username);
+    console.log(`유저 username, password: ${username},${password}`);
+    const user = await User.findOne({ username });
     if (!user) {
-      return done(null, false, { message: 'Incorrect username.' });
+      return done(null, false, { message: "Incorrect username." });
     }
 
     const isMatch = user.password == password;
     if (!isMatch) {
-      return done(null, false, { message: 'Incorrect password.' });
+      return done(null, false, { message: "Incorrect password." });
     }
     return done(null, user);
   }
